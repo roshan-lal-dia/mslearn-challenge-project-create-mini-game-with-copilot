d685e7373876c726c72752aabfb19bacae112968
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-12-01 04:34:22 +0000

    Refactor game logic and add play again option

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 import random
 
 def play_game():
-    choices = ['rock', 'paper', 'scissors']
-    computer_choice = random.choice(choices)
-    user_choice = input("Enter your choice (rock, paper, scissors): ")
+   choices = ['rock', 'paper', 'scissors']
+   computer_choice = random.choice(choices)
+   user_choice = input("Enter your choice (rock, paper, scissors): ")
 
-    print(f"Computer chose: {computer_choice}")
-    print(f"You chose: {user_choice}")
+   print(f"Computer chose: {computer_choice}")
+   print(f"You chose: {user_choice}")
 
-    if user_choice == computer_choice:
-        print("It's a tie!")
-    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
-         (user_choice == 'paper' and computer_choice == 'rock') or \
-         (user_choice == 'scissors' and computer_choice == 'paper'):
-        print("You win!")
-    else:
-        print("Computer wins!")
+   if user_choice == computer_choice:
+      print("It's a tie!")
+   elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+       (user_choice == 'paper' and computer_choice == 'rock') or \
+       (user_choice == 'scissors' and computer_choice == 'paper'):
+      print("You win!")
+   else:
+      print("Computer wins!")
+
+   play_again = input("Do you want to play again? (yes/no): ")
+   if play_again.lower() == "yes":
+      play_game()
 
 play_game()
